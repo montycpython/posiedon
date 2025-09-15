3f84bff1b68d2d9107803a8bd653553b42806356
Montavious <real.artistry@gmail.com>
2025-09-15 12:09:16 -0400

    C99

---
+++ masterc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <time.h>
+/*
+auto       break      case       char       const
+continue   default    do         double     else
+enum       extern     float      for        goto
+if         int        long       register   return
+short      signed     sizeof     static     struct
+switch     typedef    union      unsigned   void
+volatile   while
+*/
+#define PATH "C:\\Users\\orisa\\Documents\\newCode\\poseidon\\masterc.c"
+
+int main() {
+    printf("Hello, World!\n");
+    printf("File path: %s\n", PATH);
+    return 0;
+}
+
+
